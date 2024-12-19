c5a94bc5dc15ba8d5dcc8cafdae5820b28828e51
Ahmed <ahmedcodes48@gmail.com>
2024-12-19 16:14:58 +0500

    small changes

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,17 +1,10 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
-import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
 
-import "./Feed.css";
-import { API_KEY, value_converter } from "../../data";
-import { useEffect, useState } from "react";
 import moment from "moment";
+import { useEffect, useState } from "react";
+import { API_KEY, value_converter } from "../../data";
+import "./Feed.css";
 
 const Feed = ({ category }) => {
   const [data, setData] = useState([]);
